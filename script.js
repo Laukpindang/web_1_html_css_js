5b9a0d5a8ccccbf49a3d83a6bdd1ff74d59bfcbe
Laukpindang <ferdiansyahbandung3@gmail.com>
2025-01-28 09:53:34 +0700

    edit

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function addTask() {
     new Date().valueOf().toString() +
     Math.random().toString(36).substring(2, 7);
   task.classList.add('list-item');
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.addEventListener('click', () => {
+    editTask(task.id, input);
+  });
+  task.appendChild(editButton);
   const deleteButton = document.createElement('button');
   deleteButton.textContent = 'Delete';
   deleteButton.classList.add('delete-button');
@@ -20,4 +26,22 @@ function addTask() {
 function deleteTask(id) {
   const task = document.getElementById(id);
   task.remove();
+}
+
+function editTask(id, name) {
+  const button = document.getElementById('add-button');
+  const input = document.getElementById('tf-input');
+  input.value = name;
+  button.textContent = 'Edit';
+  button.addEventListener('click', () => {
+    submitEdit(id);
+  });
+}
+
+function submitEdit(id) {
+  const input = document.getElementById('tf-input').value;
+  const button = document.getElementById('add-button');
+  const item = document.getElementById(id);
+  item.textContent = input;
+  button.textContent = 'Add Task';
 }
